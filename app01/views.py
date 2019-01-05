57fbe0e52a5ed7c78059f074a228afbc483dfb0c
nick.chen <nick.chen@smartteamglb.com>
2019-01-05 23:08:05 +0800

    test

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+import datetime
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    now = datetime.datetime.now()
+    c_time = now.strftime("%Y-%m-%d %X")
+    return render(request, 'index.html', {'c_time': c_time})
